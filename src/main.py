97f738ab7c270b54bb2f136b7f1efeebf9f4d5ae
DejvDajo <davidkonderla@email.cz>
2025-10-25 04:10:09 +0200

    Update main.py
    Binded: main.menu, helper ; Issues to fix: main_menu flik while opening helper scene

--- src/main.py
+++ src/main.py
@@ -1,20 +1,50 @@
 import pygame
+import main_menu
+import helper
 
-pygame.font.init()
+main_menu_scene: bool = True
+helper_scene: bool = False
 
-window = pygame.display.set_mode((1600,900))
-width, height = window.get_size()
 
-run, clock = True, pygame.time.Clock()
-while run:
-    window.fill((255,255,255))
-    pygame.display.update()
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+def main():
+    global main_menu_scene, helper_scene
+    window = pygame.display.set_mode((1600,900), pygame.FULLSCREEN)
+    width, height = window.get_size()
+    main_menu.initialize(width, height)
+    helper.initialize(width, height)
+
+    run, clock = True, pygame.time.Clock()
+    while run:
+        pygame.display.update()
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+
+        match main_menu.report_activity():
+            case "help":
+                main_menu_scene = False
+                helper_scene = True
+            case "quit":
                 run = False
 
-pygame.quit()
+        match helper.report_activity():
+            case "help_exit":
+                main_menu_scene = True
+                helper_scene = False
+
+        if main_menu_scene:
+            main_menu.update(window)
+            main_menu.draw(window)
+        if helper_scene:
+            helper.update(window)
+            helper.draw(window)
+
+
+if __name__ == '__main__':
+    main()
+    pygame.quit()
